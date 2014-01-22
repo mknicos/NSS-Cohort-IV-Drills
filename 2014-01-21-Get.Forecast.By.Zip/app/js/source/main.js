0d28ce54be97ce94ea27cbbe5109c1199e3cb113
Matt Knicos <mknicos@gmail.com>
2014-01-22 16:02:34 -0600

    Get Forecast by Zip

---
+++ 2014-01-21-Get.Forecast.By.Zip/app/js/source/main.js
@@ -0,0 +1,30 @@
+(function(){
+  'use strict';
+
+  $(document).ready(init);
+
+  function init(){
+    $('#addZip').click(clickAddZip);
+    $('#getForecast').click(clickGetForecast);
+  }
+
+  function clickAddZip(){
+    var input = $('#inputZip').val();
+    var $option = $('<option>');
+    $option.val(input);
+    $option.text(input);
+    $('#selector').append($option);
+  }
+
+  function clickGetForecast(){
+    var zipCode = $('#selector').val();
+    var url = 'http://api.wunderground.com/api/44ef700b986d88f1/forecast/q/' + zipCode +'.json?callback=?';
+    $.getJSON(url, request);
+      
+    function request(data){
+      console.log(data);
+    }
+  }
+
+
+})();
